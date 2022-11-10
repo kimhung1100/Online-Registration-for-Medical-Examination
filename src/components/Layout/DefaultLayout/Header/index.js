0b7a2deba171e0136a805a85ca0299e4dae28d81
unknown <80261955+kimhung1100@users.noreply.github.com>
2022-11-10 22:22:11 +0700

    z

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -3,6 +3,25 @@ import styles from './Header.module.scss';
 import whiteLogo from '../../../../assets/images/white_logo.jpg';
 const cx = classNames.bind(styles);
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+    myFunction();
+};
+
+// Get the header
+var header = document.getElementById('myHeader');
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+}
 function Header() {
     return (
         <header className={cx('wrapper')}>
