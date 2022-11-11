5fd012002dcf6ff45ffce6aad2c4011e7dfc0bef
gerphan <germanphan2002@gmail.com>
2022-11-11 14:36:40 +0700

    chỉnh sửa js

---
+++ src/pages/ChuyenKhoa/ThongTinChuyenKhoa/index.js
@@ -0,0 +1,51 @@
+import "./chuyenKhoa.css"
+
+function ThongTinChuyenKhoa() {
+    return (
+        <div className="chuyen_khoa_box">
+            <div className="lam_sang">
+                <div className="anh_lam_sang">
+                    <img src="..\..\assets\images\lam_sang.jpg" alt="" />
+                    <div className="khoa_content">KHOA LÂM SÀNG</div>
+                </div>
+                <div className="noi_dung_khoa">
+                    <ul>
+                        <li><a href="">Khoa Cấp cứu</a></li>
+                        <li><a href="">Khoa Khám bệnh</a></li>
+                        <li><a href="">Khoa Nội tiết</a></li>
+                        <li><a href="">Khoa Nội tim mạch</a></li>
+                        <li><a href="">Khoa Tim mạch can thiệp</a></li>
+                        <li><a href="">Khoa Thần kinh</a></li>          
+                        <li><a href="">Khoa Tai - Mũi - Họng</a></li>
+                        <li><a href="">Khoa Phụ sản</a></li>
+                        <li><a href="">Khoa Tiêu hoá</a></li>
+                        <li><a href="">Khoa Hô hấp</a></li>
+                        <li><a href="">Khoa Mắt</a></li>
+                        <li><a href="">Khoa Tạo hình Thẩm mỹ</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="can_lam_sang">
+                <div className="anh_lam_sang">
+                    <img src="..\..\assets\images\can_lam_sang.jpg" alt="" />
+                    <div className="khoa_content">KHOA CẬN LÂM SÀNG</div>
+                </div>    
+                <div className="noi_dung_khoa">
+                    <ul>
+                        <li><a href="">Khoa Chẩn đoán hình ảnh</a></li>
+                        <li><a href="">Khoa giải phẫu bệnh</a></li>
+                        <li><a href="">Khoa Dinh dưỡng - Tiết chế</a></li>
+                        <li><a href="">Khoa Nội soi</a></li>
+                        <li><a href="">Khoa Xét nghiệm</a></li>
+                        <li><a href="">Khoa Dược</a></li>
+                        <li><a href="">Khoa Vi sinh</a></li>
+                        <li><a href="">Khoa Y học hạt nhân</a></li>
+                    </ul>
+                </div>
+            </div>    
+        </div>
+    );
+}
+
+export default ThongTinChuyenKhoa;
