bc36c1e44b1aecd8f4b2f4a32bafc8757fcaf2c4
quocan0301 <ananpro03103@gmail.com>
2022-11-12 12:24:56 +0700

    font+dangnhap+dangki

--- src/pages/DangKy/index.js
+++ src/pages/DangKy/index.js
@@ -1,5 +1,56 @@
+
+
 function DangKy() {
-    return <h2>DangKy</h2>;
+    return(
+        <section>
+            <div className="quaylai"><a href="#" className="previous">&#8592;</a></div>
+            <div className="noi-dung">
+                <div className="form">
+                    <h2>Form đăng kí</h2>
+                    <form action="">
+                        <div className="input-form">
+                            <label for="phone">Số điện thoại:</label><input type="text" name="" placeholder="+84"></input>
+                        </div>
+                        <div className="input-form">
+                            <label for="username">Tên tài khoản:</label><input type="text" id="username" name="username"
+                                placeholder=""></input>
+                        </div>
+                        <div className="input-form">
+                            <label for="password">Password:</label><input type="password" id="password" name="password"
+                                placeholder=""></input>
+                            <p> 6-14 kí tự, kết hợp chữ cái và số</p>
+                        </div>
+                        <ul className="gender">
+                            <li><label for="gender">Giới tính:</label> </li>
+                            <li><label for="male">Nam</label>
+                                <input type="radio" name="gender" id="male" value="male" checked></input>
+                            </li>
+                            <li><label for="female">Nữ</label>
+                                <input type="radio" name="gender" id="female" value="female"></input>
+                            </li>
+                        </ul>
+                    </form>
+                    <ul className="icon-dang-nhap">
+                        <li>
+                            <i className="fa fa-facebook" aria-hidden="true"></i>
+                            <p><a href="http://127.0.0.1:5500/dangki.html">Liên kết với facebook</a></p>
+                        </li>
+                        <li>
+                            <i className="fa fa-google" aria-hidden="true"></i>
+                            <p><a href="http://127.0.0.1:5500/dangki.html">Liên kết với google</a></p>
+                        </li>
+                        <li>
+                            <i className="fa fa-twitter" aria-hidden="true"></i>
+                            <p><a href="http://127.0.0.1:5500/dangki.html">Liên kết với twitter</a></p>
+                        </li>
+                    </ul>
+                    <div className="input-form1">
+                        <input type="submit" value="Xác nhận" style="height:45px; width:200px" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default DangKy;
--- src/pages/DangNhap/index.js
+++ src/pages/DangNhap/index.js
@@ -1,5 +1,40 @@
+import '.dangnhap.scss'
+import '../../assets/images/dangnhap.png'
+import '../../assets/fonts/Archivo-VariableFont_wdth,wght.ttf'
+
 function DangNhap() {
-    return <h2>Dang Nha</h2>;
+    return (
+        <section>
+            <div className="quaylai"><a href="#" className="previous">&#8592;</a></div>
+            <div className="noi-dung">
+                <div className="form">
+                    <h2>Đăng nhập</h2>
+                    <h3>Vui lòng nhập số điện thoại để tiếp tục</h3>
+                    <form action="">
+                        <div className="input-form">
+                            <input type="text" name="" placeholder="+84"></input>
+                        </div>
+
+                        <div className="input-form">
+                            <input type="submit" value="Tiếp tục"></input>
+                        </div>
+                        <div className="input-form">
+                            <p>Bạn Chưa Có Tài Khoản? <a href="http://127.0.0.1:5500/dangki.html">Đăng Ký</a></p>
+                        </div>
+                    </form>
+                    <h3>Đăng Nhập Bằng Mạng Xã Hội</h3>
+                    <ul class="icon-dang-nhap">
+                        <li><i className="fa fa-facebook" aria-hidden="true"></i></li>
+                        <li><i className="fa fa-google" aria-hidden="true"></i></li>
+                        <li><i className="fa fa-twitter" aria-hidden="true"></i></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="img-bg">
+                <img src="images/dangnhap.png"></img>
+            </div>
+        </section>       
+    );
 }
 
 export default DangNhap;
