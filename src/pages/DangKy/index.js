2355935e7fcf5ec8a8be5462ab604d711d9ae4b1
quocan0301 <ananpro03103@gmail.com>
2022-11-25 20:27:35 +0700

    change dk dn

--- src/pages/DangKy/index.js
+++ src/pages/DangKy/index.js
@@ -1,27 +1,28 @@
-import './dangki.scss'
+import styles from './dangki.scss'
+import classNames from 'classnames/bind';
 import '../../assets/fonts/Archivo-VariableFont_wdth,wght.ttf'
-
+const cx = classNames.bind(styles);
 function DangKy() {
     return(
-        <section>
-            <div className="quaylai"><a href="./" className="previous">&#8592;</a></div>
-            <div className="noi-dung1">
-                <div className="form">
+        <section className={cx("container2")}>
+            <div className={cx("quaylai")}><a href="./" className="previous">&#8592;</a></div>
+            <div className={cx("noi-dung1")}>
+                <div className={cx("form")}>
                     <h2>Form đăng kí </h2>
                     <form action="">
-                        <div className="input-form">
+                        <div className={cx("input-form")}>
                             <label for="phone">Số điện thoại:</label><input type="text" name="" placeholder="+84"></input>
                         </div>
-                        <div className="input-form">
+                        <div className={cx("input-form")}>
                             <label for="username">Tên tài khoản:</label><input type="text" id="username" name="username"
                                 placeholder=""></input>
                         </div>
-                        <div className="input-form">
+                        <div className={cx("input-form")}>
                             <label for="password">Password:</label><input type="password" id="password" name="password"
                                 placeholder=""></input>
                             <p> 6-14 kí tự, kết hợp chữ cái và số</p>
                         </div>
-                        <ul className="gender">
+                        <ul className={cx("gender")}>
                             <li><label for="gender">Giới tính:</label> </li>
                             <li><label for="male">Nam</label>
                                 <input type="radio" name="gender" id="male" value="male" checked></input>
@@ -31,21 +32,21 @@ function DangKy() {
                             </li>
                         </ul>
                     </form>
-                    <ul className="icon-dang-nhap">
+                    <ul className={cx("icon-dang-nhap")}>
                         <li>
-                            <i className="fa fa-facebook" aria-hidden="true"></i>
+                            <i className={cx("fa fa-facebook")} aria-hidden="true"></i>
                             <p><a href="">Liên kết với facebook</a></p>
                         </li>
                         <li>
-                            <i className="fa fa-google" aria-hidden="true"></i>
+                            <i className={cx("fa fa-google")} aria-hidden="true"></i>
                             <p><a href="">Liên kết với google</a></p>
                         </li>
                         <li>
-                            <i className="fa fa-twitter" aria-hidden="true"></i>
+                            <i className={cx("fa fa-twitter")} aria-hidden="true"></i>
                             <p><a href="">Liên kết với twitter</a></p>
                         </li>
                     </ul>
-                    <div className="input-form1">
+                    <div className={cx("input-form1")}>
                         <input type="submit" value="Xác nhận" ></input>
                     </div>
                 </div>
--- src/pages/DangNhap/index.js
+++ src/pages/DangNhap/index.js
@@ -1,36 +1,37 @@
-import './dangnhap.scss'
+import styles from './dangnhap.scss'
+import classNames from 'classnames/bind';
 import img from '../../assets/images/dangnhap.png'
 import '../../assets/fonts/Archivo-VariableFont_wdth,wght.ttf'
-
+const cx = classNames.bind(styles);
 function DangNhap() {
     return (
-        <section>
-            <div className="quaylai"><a href="./" className="previous">&#8592;</a></div>
-            <div className="noi-dung">
-                <div className="form">
+        <section className={cx('container1')}>
+            <div className={cx('quaylai')}><a href="./" className={cx('previous')}>&#8592;</a></div>
+            <div className='noi-dung'>
+                <div className='form'>
                     <h2>Đăng nhập</h2>
                     <h3>Vui lòng nhập số điện thoại để tiếp tục</h3>
-                    <form action="">
-                        <div className="input-form">
+                    <form action=''>
+                        <div className={cx('input-form')}>
                             <input type="text" name="" placeholder="+84"></input>
                         </div>
 
-                        <div className="input-form">
+                        <div className={cx("input-form")}>
                             <input type="submit" value="Tiếp tục"></input>
                         </div>
-                        <div className="input-form">
+                        <div className={cx("input-form")}>
                             <p>Bạn Chưa Có Tài Khoản? <a href="http://localhost:3000/dang-ky">Đăng Ký</a></p>
                         </div>
                     </form>
                     <h3>Đăng Nhập Bằng Mạng Xã Hội</h3>
-                    <ul class="icon-dang-nhap">
-                        <li><i className="fa fa-facebook" aria-hidden="true"></i></li>
-                        <li><i className="fa fa-google" aria-hidden="true"></i></li>
-                        <li><i className="fa fa-twitter" aria-hidden="true"></i></li>
+                    <ul class={cx("icon-dang-nhap")}>
+                        <li><i className={cx("fa fa-facebook")} aria-hidden="true"></i></li>
+                        <li><i className={cx("fa fa-google")} aria-hidden="true"></i></li>
+                        <li><i className={cx("fa fa-twitter")} aria-hidden="true"></i></li>
                     </ul>
                 </div>
             </div>
-            <div className="img-bg">
+            <div className={cx("img-bg")}>
                 <img src={img} value="image"></img>
             </div>
         </section>       
