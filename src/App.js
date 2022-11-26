1007363aa7eb80e675d687b494930b18129ece50
chau <nguyenthiminhchau.bhn.92.06@gmail.com>
2022-11-26 18:41:28 +0700

    useContext

--- src/App.js
+++ src/App.js
@@ -3,19 +3,35 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from './routes';
 import { DefaultLayout } from './components/Layout';
 function App() {
+    const [user,setUser] = useState({
+        key: Number,
+        login: Boolean,
+        userName:"",
+        password:"",
+        phone:"",
+        gender:"",
+        role: 'Khach Hang'
+    });
+    const value = useMemo (() => ({user, setUser}), [user,setUser]);
     return (
         <Router>
             <div className="App">
+            <UserContext.Provider value={[user,setUser]}>
                 <Routes>
                     {publicRoutes.map((route, index) => {
                         const Page = route.component;
+
                         let Layout = DefaultLayout;
                         if (route.layout) {
                             Layout = route.layout;
                         } else if (route.layout === null) {
                             Layout = Fragment;
                         }
-
+                        {
+                            /* if (route.path === './gioi-thieu') {
+                            handleTrangChu();
+                        } */
+                        }
                         return (
                             <Route
                                 key={index}
@@ -29,6 +45,7 @@ function App() {
                         );
                     })}
                 </Routes>
+                </UserContext.Provider>
             </div>
         </Router>
     );
