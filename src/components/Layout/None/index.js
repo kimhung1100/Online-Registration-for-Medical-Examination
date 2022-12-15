7033eaf1800e0bb774a53eb54c06092732a35401
unknown <80261955+kimhung1100@users.noreply.github.com>
2022-12-15 12:55:02 +0700

    header admin

---
+++ src/components/Layout/None/index.js
@@ -0,0 +1,15 @@
+import Footer from '../DefaultLayout/Footer';
+import classNames from 'classnames/bind';
+import styles from './NotLinearBar.module.scss';
+
+const cx = classNames.bind(styles);
+function NotLinearBar({ children }) {
+    return (
+        <div className={cx('wrapper')}>
+            <div className="content">{children}</div>
+            <Footer />
+        </div>
+    );
+}
+
+export default NotLinearBar;
--- src/pages/NhapHoSoBacSi/index.js
+++ src/pages/NhapHoSoBacSi/index.js
@@ -1,8 +1,26 @@
 import classNames from 'classnames/bind';
-
+import React, { useState, useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import styles from './NhapHoSoBacSi.module.scss';
+import { UserContext } from '../../components/UserContext';
+import whiteLogo from '../../assets/images/white_logo.jpg';
 const cx = classNames.bind(styles);
 function NhapHoSoBacSi() {
+    const [user, setUser] = useContext(UserContext);
+    const navigate = useNavigate();
+    const [QuanLyTaiKhoan, setQuanLyTaiKhoan] = useState(false);
+    const [ThongKeLuotTruyCap, setThongKeLuotTruyCap] = useState(false);
+    const [NhapHoSoBacSi, setNhapHoSoBacSi] = useState(false);
+
+    const handleThongKeLuotTruyCap = () => {
+        setThongKeLuotTruyCap = true;
+        setQuanLyTaiKhoan = false;
+    };
+    const handleQuanLyTaiKhoan = () => {
+        setQuanLyTaiKhoan = true;
+        setThongKeLuotTruyCap = false;
+    };
+
     return (
         <div id="NhapHoSoBacSi">
             <p>NhapHoSoBacSi</p>
