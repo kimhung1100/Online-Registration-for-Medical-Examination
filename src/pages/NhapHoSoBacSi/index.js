d78ca0a94d53b1e47a9ed1473eaacafe64733868
chau <nguyenthiminhchau.bhn.92.06@gmail.com>
2022-12-15 13:02:11 +0700

    nhaphosobs

--- src/pages/NhapHoSoBacSi/index.js
+++ src/pages/NhapHoSoBacSi/index.js
@@ -4,23 +4,7 @@ import styles from './NhapHoSoBacSi.module.scss';
 const cx = classNames.bind(styles);
 function NhapHoSoBacSi() {
     return (
-        <div class="container">
-            <div class="row">
-            <div class="col-md-6 offset-md-3">
-            <form action="/import-excel" enctype="multipart/form-data" method="post">
-                <input
-                class="form-control"
-                type="file"
-                id="formFileMultiple"
-                name="import-excel"
-                accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
-                multiple
-                />
-                <button type="submit" class="btn btn-dark" value="Store File"></button>
-            </form>
-            </div>
-            </div>
-        </div>
+        <div>div</div>
     );
 }
 {/* <div class="mb-3">
